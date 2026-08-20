cfcce60349211fe79511078a7a8d47a66b515893
Torkoal105 <collin.zuiderwijk@ziggo.nl>
2026-08-20 14:09:23 +0200

    Update parallel_nmap_portscan_only.py

--- parallel_nmap_portscan_only.py
+++ parallel_nmap_portscan_only.py
@@ -10,7 +10,7 @@ view, but each host scan runs in parallel under the hood).
 Usage examples:
     # Scan a /24 subnet (up to 254 hosts), nmap args "-sV -sT -T5 -p- --host-timeout 30s --max-rtt-timeout 30s --initial-rtt-timeout 30s"
     # normal nmap tcp synchronously
-    python3 parallel_nmap_portscan_only.py -t 192.168.0.0/24 -a "-sV -sT -T5 -p- --host-timeout 30s --max-rtt-timeout 30s --initial-rtt-timeout 30s"
+    python3 parallel_nmap_portscan_only.py -t 192.168.100.0/24 -a "-sV -sT -T5 -p- --host-timeout 30s --max-rtt-timeout 30s --initial-rtt-timeout 30s"
 """
 
 import argparse
